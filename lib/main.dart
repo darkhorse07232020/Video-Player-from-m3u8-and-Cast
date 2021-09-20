e891758eaa79c9933eebf388fc5556a858653777
Sergej <darkhorse0723@gmail.com>
2021-09-20 18:13:04 +0300

    create cast button on Headbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_video_cast/flutter_video_cast.dart';
 import 'package:video_player/video_player.dart';
 import 'package:video_player_m3u8_and_casting/components/video_player.dart';
 
@@ -12,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Testing',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Cast Sample'),
     );
   }
 }
@@ -31,20 +32,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late ChromeCastController _controller;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: VideoItem(
-          videoPlayerController: VideoPlayerController.network(
-            "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+        actions: [
+          ChromeCastButton(
+            onButtonCreated: (controller) {
+              setState(() => _controller = controller);
+              _controller.addSessionListener();
+            },
+            onSessionStarted: () {
+              _controller.loadMedia(
+                  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+            },
           ),
-          looping: true,
-          autoplay: true,
-        ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            child: VideoItem(
+              videoPlayerController: VideoPlayerController.network(
+                "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+              ),
+              looping: true,
+              autoplay: true,
+            ),
+          ),
+        ],
       ),
     );
   }
